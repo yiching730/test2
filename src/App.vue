07c6d2abd3530fbfc65d76c2b3cb3561c858c8b3
jing <bcs109113@gm.ntcu.edu.tw>
2021-10-17 11:17:37 +0800

    card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,55 @@
 <template>
   <div id="app">
+   <Card :cardData="cardData" />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+  export default {
+    name: 'app',
+    components:{
+      Card
+    },
+    data(){
+      return{
+        cardData:[
+          {
+            name:'Adam',
+            birthday:'1995.08.12',
+            mail:'adam@xxx.com'
+          },
+          {
+            name:'Jack',
+            birthday:'1980.02.23',
+            mail:'jack@xxx.com'
+          },
+          {
+            name:'Candy',
+            birthday:'1982.05.06',
+            mail:'candy@xxx.com'
+          },
+          {
+            name:'Louis',
+            birthday:'1984.11.21',
+            mail:'loui@xxx.com'
+          },
+          {
+            name:'Lurry',
+            birthday:'1982.01.17',
+            mail:'lurry@xxx.com'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style></style>
+
+<!--  原來程式碼
+<template>
+  <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -26,3 +76,5 @@ export default {
   margin-top: 60px;
 }
 </style>
+-->
+ 
