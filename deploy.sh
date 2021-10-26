bd49e92bf60a786562a246540c43fffdc142049e
jing <bcs109113@gm.ntcu.edu.tw>
2021-10-26 23:25:44 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git add -A
 git commit -m 'deploy'
 
 # 將 dist資料夾中的內容推送至遠端 hexWeek6Demo的 gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f https://github.com/yiching730/vue_learning.git master:gh-pages
+git push -f https://github.com/yiching730/test2.git master:gh-pages
 cd -
 
 # 執行指令，在終端機輸入 deploy.sh
